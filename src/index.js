4b79c994db651dac78a186f650e73415efe98cac
karen-tacode <karen@tacode.mx>
2020-01-22 13:35:18 -0600

    Funcion que cambia el color del icono del menu dependiendo la seccion en la que se encuentre. seccion home completa

--- src/index.js
+++ src/index.js
@@ -1,21 +1,57 @@
+/* ----------------- General elements ---------------------- */
+
+const sectionLinksObj = document.querySelectorAll('.section-links');
+const appPages = document.querySelectorAll('.pages');
+
+/* ----------------- Menu elements ---------------------- */
+
 const openMenuIcon = document.getElementById('open-menu-icon');
+const closeMenuIcon = document.getElementById('close-menu-icon');
 const hamburgerIconSvgPath1 = document.getElementById('hamburger-svg-path-1');
 const hamburgerIconSvgPath2 = document.getElementById('hamburger-svg-path-2');
 const menu = document.getElementById('menu');
+
+/* ----------------- Home elements ---------------------- */
+
+const homeSection = document.getElementById('home');
+
+/* ----------------- Functions ---------------------- */
+
+function hamburgerIconColor() {
+  let iconDisplayStyle = window.getComputedStyle(homeSection, null).getPropertyValue("display");
+    if (iconDisplayStyle === 'grid') {
+        hamburgerIconSvgPath1.setAttribute('stroke', '#FEFEFE');
+        hamburgerIconSvgPath2.setAttribute('fill', '#FFFFFF');
+    } else if(iconDisplayStyle === 'none') {
+        hamburgerIconSvgPath1.removeAttribute('stroke', '#272424');
+        hamburgerIconSvgPath2.setAttribute('fill', '#000000');
+    }
+}
+hamburgerIconColor();
+
+/* ----------------- DOM events ---------------------- */
 
 openMenuIcon.addEventListener('click', (event) => {
     event.preventDefault();
     menu.classList.add('active');
 });
 
-const homeSection = document.getElementById('home');
-const homeLink = document.getElementById('home-link');
+closeMenuIcon.addEventListener('click', () => {
+    event.preventDefault();
+    menu.classList.remove('active');
+});
 
-function hamburgerIconColor () {
-    if(homeSection.classList.contains('active')){
-       hamburgerIconSvgPath1.setAttribute('stroke', '#FEFEFE');
-       hamburgerIconSvgPath2.setAttribute('fill', '#FFFFFF') 
-    }
-}
-
-hamburgerIconColor();
+sectionLinksObj.forEach(element => {
+    element.addEventListener('click', (event) => {
+        event.preventDefault();
+        menu.classList.remove('active');
+        appPages.forEach(page => {
+            hamburgerIconColor();
+            if (event.target.classList.contains(page.id)) {
+                page.classList.add('active');
+            } else {
+                page.classList.remove('active');
+            }
+        })
+    })
+})
