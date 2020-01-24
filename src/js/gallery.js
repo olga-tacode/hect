3b595fb3df63e1e9dc35356653f86042c828f4dd
karen-tacode <karen@tacode.mx>
2020-01-24 16:46:01 -0600

    modal an carousel for branding section

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,70 +1,151 @@
-const gallery = document.getElementById("gallery");
-const photoCategoriesLinks = document.querySelectorAll(".portfolio-categories");
+const gallery = document.getElementById('gallery');
+const photoCategoriesLinks = document.querySelectorAll('.portfolio-categories');
+const modal = document.querySelector('.modal');
+const carousel = document.querySelector('.carousel');
+const closeModalBttn = document.getElementById('close-modal');
+let slideIndex = 1;
+const changingBttns = document.querySelectorAll('.changing-bttns');
 
 function obtainingJsonData(id) {
-  fetch("./src/photos.json")
-    .then(response => {
-      return response.json();
-    })
-    .then(photosCollection => {
-      filterElements(photosCollection, id);
-    });
-}
-
-const filterElements = (photos, id) => {
-  let filteredArr = photos.filter(photo => photo.category === id);
-  loopingFilteredArr(filteredArr);
+    fetch('./src/photos.json')
+        .then(response => {
+            return response.json();
+        })
+        .then(photosCollection => {
+            filterElements(photosCollection, id);
+        });
 };
 
+const filterElements = (photos, id) => {
+    let filteredArr = photos.filter(photo => photo.category === id);
+    if (id != 'branding') {
+        loopingFilteredArr(filteredArr);
+    } else {
+        handleBranding(filteredArr);
+    }
+};
+
+/* --------------------------- For every category, except branding -------------------------------- */
+
 const loopingFilteredArr = arr => {
-  gallery.innerHTML = "";
-  arr.forEach(element => {
-    paintPhotos(element);
-  });
+    gallery.innerHTML = "";
+    arr.forEach(element => {
+        paintPhotos(element);
+    });
 };
 
 const paintPhotos = photo => {
-  let dataToHTML = `
+    let dataToHTML = `
     <div class="container ${photo.class}">
         <button id="close-photo-bttn">X</button>
         <img class="item" src="${photo.url}"/>
     </div>`;
-  gallery.insertAdjacentHTML("beforeend", dataToHTML);
-  /* openPhoto(); */
+    gallery.insertAdjacentHTML('beforeend', dataToHTML);
+    /* openPhoto(); */
 };
 
-/* const openPhoto = () => {
-    const imagesContainer = document.querySelectorAll('.container');
-    const closePhotoBttn = document.getElementById('close-photo-bttn');
-    imagesContainer.forEach(element => {
-        element.addEventListener('click', (event) => {
+/* --------------------------- For branding section -------------------------------- */
+
+const handleBranding = arr => {
+    gallery.innerHTML = '';
+    const brandingCategories = `
+    <div class="widest branding blueB" id="publicaciones">Publicaciones</div>
+    <div class="taller branding blackB" id="cocacola">Coca-Cola</div>
+    <div class="small branding blueB" id="capufe">CAPUFE</div>
+    <div class="small branding blueB" id="kellogs">Kellog's</div>
+    `
+    gallery.insertAdjacentHTML('beforeend', brandingCategories);
+    const brandingCategoriesArr = document.querySelectorAll('.branding');
+    handlingCategoriesOnClick(brandingCategoriesArr, arr);
+};
+
+const handlingCategoriesOnClick = (categoriesCollection, photosCollection) => {
+    categoriesCollection.forEach(category => {
+        category.addEventListener('click', event => {
             event.preventDefault();
-            element.classList.add('container-active');
-            event.target.classList.add('img-active');
-            closePhotoBttn.classList.add('active');
-            console.log(element)
-            closeModal(element, event.target, closePhotoBttn);
-        })
+            filteringBrandinPhotos(category.id, photosCollection);
+        });
     });
-} */
+};
+
+const filteringBrandinPhotos = (id, photos) => {
+    let filteredArr = photos.filter(photo => photo.subcategory === id);
+    paintBrandingPhotos(filteredArr);
+};
+
+/* --------------------------- Modal of branding section + carousel -------------------------------- */
+
+const paintBrandingPhotos = arr => {
+    modal.classList.add('active');
+    arr.forEach(element => {
+        let dataToCarousel = `
+        <img class = "mySlides" src="${element.url}" style="width:100%; object-fit: cover;"/>`
+        carousel.insertAdjacentHTML('beforeend', dataToCarousel);
+    });
+    showDivs(slideIndex);
+    changeImg();
+    closeModal();
+};
+
+const closeModal = () => {
+    closeModalBttn.addEventListener('click', () => {
+        modal.classList.remove('active');
+        carousel.innerHTML = '';
+    })
+};
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    let i;
+    const x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}
+
+const changeImg = () => {
+    changingBttns.forEach(bttn => {
+        bttn.addEventListener('click', (event) => {
+            if (event.target.id === 'prev') {
+                plusDivs(-1);
+            } else {
+                plusDivs(1);
+            }
+        })
+    })
+};
+
+/* --------------------------- Function that changes link color when is activated -------------------------------- */
 
 const coloringLinksCategories = (arr, link) => {
-  arr.forEach(element => {
-    if (element.classList.contains("active")) {
-      element.classList.remove("active");
-    }
-  });
-  link.classList.add("active");
+    arr.forEach(element => {
+        if (element.classList.contains('active')) {
+            element.classList.remove('active');
+        }
+    });
+    link.classList.add('active');
 };
 
+/* --------------------------- Function that triggers the fetch function -------------------------------- */
+
 photoCategoriesLinks.forEach(category => {
-  category.addEventListener("click", event => {
-    event.preventDefault();
-    obtainingJsonData(event.target.id);
-    coloringLinksCategories(photoCategoriesLinks, event.target);
-  });
+    category.addEventListener('click', event => {
+        event.preventDefault();
+        obtainingJsonData(event.target.id);
+        coloringLinksCategories(photoCategoriesLinks, event.target);
+    });
 });
 
 window.onload = () => {
-  obtainingJsonData("fashion");
+    obtainingJsonData('fashion');
 };
