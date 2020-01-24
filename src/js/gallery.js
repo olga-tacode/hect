cb9268bf027fb8af169e81e89d68e5218f13e652
olga-tacode <olga@tacode.mx>
2020-01-24 10:01:56 -0600

    adding changes for pull

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,70 +1,70 @@
-const gallery = document.getElementById('gallery');
-const photoCategoriesLinks = document.querySelectorAll('.portfolio-categories');
+const gallery = document.getElementById("gallery");
+const photoCategoriesLinks = document.querySelectorAll(".portfolio-categories");
 
 function obtainingJsonData(id) {
-    fetch('./src/photos.json')
-        .then(response => {
-            return response.json();
-        })
-        .then(photosCollection => {
-            filterElements(photosCollection, id);
-        })
+  fetch("./src/photos.json")
+    .then(response => {
+      return response.json();
+    })
+    .then(photosCollection => {
+      filterElements(photosCollection, id);
+    });
 }
 
 const filterElements = (photos, id) => {
-    let filteredArr = photos.filter(photo => photo.category === id);
-    loopingFilteredArr(filteredArr);
-}
+  let filteredArr = photos.filter(photo => photo.category === id);
+  loopingFilteredArr(filteredArr);
+};
 
-const loopingFilteredArr = (arr) => {
-    gallery.innerHTML = "";
-    arr.forEach(element => {
-        paintPhotos(element);
-    })
-}
+const loopingFilteredArr = arr => {
+  gallery.innerHTML = "";
+  arr.forEach(element => {
+    paintPhotos(element);
+  });
+};
 
-const paintPhotos = (photo) => {
-    let dataToHTML = `
+const paintPhotos = photo => {
+  let dataToHTML = `
     <div class="container ${photo.class}">
         <button id="close-photo-bttn">X</button>
         <img class="item" src="${photo.url}"/>
-    </div>`
-    gallery.insertAdjacentHTML('beforeend', dataToHTML);
-    /* openPhoto(); */
-}
+    </div>`;
+  gallery.insertAdjacentHTML("beforeend", dataToHTML);
+  /* openPhoto(); */
+};
 
 const openPhoto = () => {
-    const imagesContainer = document.querySelectorAll('.container');
-    const closePhotoBttn = document.getElementById('close-photo-bttn');
-    imagesContainer.forEach(element => {
-        element.addEventListener('click', (event) => {
-            event.preventDefault();
-            element.classList.add('container-active');
-            event.target.classList.add('img-active');
-            closePhotoBttn.classList.add('active');
-            console.log(element)
-            closeModal(element, event.target, closePhotoBttn);
-        })
+  const imagesContainer = document.querySelectorAll(".container");
+  const closePhotoBttn = document.getElementById("close-photo-bttn");
+  imagesContainer.forEach(element => {
+    element.addEventListener("click", event => {
+      event.preventDefault();
+      element.classList.add("container-active");
+      event.target.classList.add("img-active");
+      closePhotoBttn.classList.add("active");
+      console.log(element);
+      closeModal(element, event.target, closePhotoBttn);
     });
-}
+  });
+};
 
 const coloringLinksCategories = (arr, link) => {
-    arr.forEach(element => {
-        if (element.classList.contains('active')) {
-            element.classList.remove('active');
-        }
-    });
-    link.classList.add('active');
-}
+  arr.forEach(element => {
+    if (element.classList.contains("active")) {
+      element.classList.remove("active");
+    }
+  });
+  link.classList.add("active");
+};
 
 photoCategoriesLinks.forEach(category => {
-    category.addEventListener('click', (event) => {
-        event.preventDefault();
-        obtainingJsonData(event.target.id);
-        coloringLinksCategories(photoCategoriesLinks, event.target);
-    })
+  category.addEventListener("click", event => {
+    event.preventDefault();
+    obtainingJsonData(event.target.id);
+    coloringLinksCategories(photoCategoriesLinks, event.target);
+  });
 });
 
 window.onload = () => {
-    obtainingJsonData('fashion');
-}
+  obtainingJsonData("fashion");
+};
