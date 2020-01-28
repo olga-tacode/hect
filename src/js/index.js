2868f81be2a2a5ae46f29ed035cba8bc182f4691
karen-tacode <karen@tacode.mx>
2020-01-28 13:57:10 -0600

    desktop menu

--- src/js/index.js
+++ src/js/index.js
@@ -5,11 +5,16 @@ const appPages = document.querySelectorAll('.pages');
 
 /* ----------------- Menu elements ---------------------- */
 
+const header = document.querySelector('header');
 const openMenuIcon = document.getElementById('open-menu-icon');
 const closeMenuIcon = document.getElementById('close-menu-icon');
 const hamburgerIconSvgPath1 = document.getElementById('hamburger-svg-path-1');
 const hamburgerIconSvgPath2 = document.getElementById('hamburger-svg-path-2');
 const menu = document.getElementById('menu');
+const logoFilling = document.getElementById('logo-filling');
+const logoStroke = document.getElementById('logo-stroke');
+const iconPathOne = document.querySelectorAll('.icon-path-one');
+const iconPathTwo = document.querySelectorAll('.icon-path-two');
 
 /* ----------------- Home elements ---------------------- */
 
@@ -23,27 +28,75 @@ const footer = document.querySelector('footer');
 /* ----------------- Functions ---------------------- */
 
 function hamburgerIconColor() {
-  let iconDisplayStyle = window.getComputedStyle(homeSection, null).getPropertyValue("display");
+    let iconDisplayStyle = window.getComputedStyle(homeSection, null).getPropertyValue("display");
     if (iconDisplayStyle === 'grid') {
         hamburgerIconSvgPath1.setAttribute('stroke', '#FEFEFE');
         hamburgerIconSvgPath2.setAttribute('fill', '#FFFFFF');
-    } else if(iconDisplayStyle === 'none') {
+    } else if (iconDisplayStyle === 'none') {
         hamburgerIconSvgPath1.setAttribute('stroke', '#272424');
         hamburgerIconSvgPath2.setAttribute('fill', '#000000');
     }
 }
 hamburgerIconColor();
 
-function displayingFooter () {
-    if (homeSection.classList.contains('active')){
+let w =
+  document.documentElement.clientWidth ||
+  document.body.clientWidth ||
+  window.innerWidth;
+
+window.addEventListener(
+    "resize",
+    () => {
+        w =
+            document.documentElement.clientWidth ||
+            document.body.clientWidth ||
+            window.innerWidth;
+        displayingFooter(w);
+    },
+    false
+);
+
+function displayingFooter(w) {
+    if (homeSection.classList.contains('active')) {
+        header.style.background = 'transparent';
         footer.style.display = 'none';
+        if (w <= 1024) {
+            logoFilling.setAttribute('fill', '');
+            logoStroke.setAttribute('stroke', '');
+            changeIconPathColorOthers(iconPathOne, iconPathTwo);
+        } else {
+            logoFilling.setAttribute('fill', '#ffffff');
+            logoStroke.setAttribute('stroke', '#ffffff');
+            changeIconPathColorHome(iconPathOne, iconPathTwo);
+        }
     } else {
+        header.style.background = '#ffffff';
         footer.style.display = 'grid';
+        logoFilling.setAttribute('fill', '#000000');
+        logoStroke.setAttribute('stroke', '#000000');
+        changeIconPathColorOthers(iconPathOne, iconPathTwo);
     }
 }
 
 displayingFooter();
 
+function changeIconPathColorHome(arrOne, arrTwo) {
+    arrOne.forEach(element => {
+        element.setAttribute('fill', '#ffffff');
+    });
+    arrTwo.forEach(element => {
+        element.setAttribute('fill', '#000000');
+    });
+};
+
+function changeIconPathColorOthers(arrOne, arrTwo) {
+    arrOne.forEach(element => {
+        element.setAttribute('fill', '#000000');
+    });
+    arrTwo.forEach(element => {
+        element.setAttribute('fill', '#ffffff');
+    });
+};
 /* ----------------- DOM events ---------------------- */
 
 openMenuIcon.addEventListener('click', (event) => {
@@ -79,4 +132,3 @@ scrollBttn.addEventListener('click', (event) => {
     portfolioSection.classList.add('active');
     hamburgerIconColor();
 });
-
