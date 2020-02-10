9ae2ff8810a540d4ef67553e36863ef8109068d8
olga-tacode <olga@tacode.mx>
2020-02-10 16:43:32 -0600

    adding changes on menu

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,191 +1,191 @@
-const gallery = document.getElementById('gallery');
-const photoCategoriesLinks = document.querySelectorAll('.portfolio-categories');
-const modal = document.querySelector('.modal');
-const carousel = document.querySelector('.carousel');
-const closeModalBttn = document.getElementById('close-modal');
-const portfolioLink = document.getElementById('portfolio-link');
-const scrollButton = document.getElementById('scroll-bttn');
-const fashionLink = document.getElementById('fashion');
+const gallery = document.getElementById("gallery");
+const photoCategoriesLinks = document.querySelectorAll(".portfolio-categories");
+const modal = document.querySelector(".modal");
+const carousel = document.querySelector(".carousel");
+const closeModalBttn = document.getElementById("close-modal");
+const portfolioLink = document.getElementById("portfolio-link");
+const socialListone = document.getElementById("social-list");
+const scrollButton = document.getElementById("scroll-bttn");
+const fashionLink = document.getElementById("fashion");
 let slideIndex;
-const prev = document.getElementById('prev');
-const next = document.getElementById('next');
-const counter = document.getElementById('counter');
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const counter = document.getElementById("counter");
 
 function obtainingJsonData(id) {
-    fetch('./src/photos.json')
-        .then(response => {
-            return response.json();
-        })
-        .then(photosCollection => {
-            filterElements(photosCollection, id);
-        });
-};
+  fetch("./src/photos.json")
+    .then(response => {
+      return response.json();
+    })
+    .then(photosCollection => {
+      filterElements(photosCollection, id);
+    });
+}
 
 const filterElements = (photos, id) => {
-    let filteredArr = photos.filter(photo => photo.category === id);
-    if (id != 'branding') {
-        loopingFilteredArr(filteredArr);
-    } else {
-        handleBranding(filteredArr);
-    }
+  let filteredArr = photos.filter(photo => photo.category === id);
+  if (id != "branding") {
+    loopingFilteredArr(filteredArr);
+  } else {
+    handleBranding(filteredArr);
+  }
 };
 
 /* --------------------------- For every category, except branding -------------------------------- */
 
 const loopingFilteredArr = arr => {
-    gallery.innerHTML = "";
-    arr.forEach(element => {
-        paintPhotos(element);
-    });
-    openPhoto(arr);
+  gallery.innerHTML = "";
+  arr.forEach(element => {
+    paintPhotos(element);
+  });
+  openPhoto(arr);
 };
 
 const paintPhotos = photo => {
-    let dataToHTML = `
+  let dataToHTML = `
     <div class="container ${photo.class} ${photo.category}" id="${photo.num}">
         <img class="item" src="${photo.url}"/>
     </div>`;
-    gallery.insertAdjacentHTML('beforeend', dataToHTML);
+  gallery.insertAdjacentHTML("beforeend", dataToHTML);
 };
 
-const openPhoto = (photosArr) => {
-    let arr = document.querySelectorAll('.container');
-    arr.forEach(element => {
-        element.addEventListener('click', (event) => {
-            event.preventDefault();
-            paintBrandingPhotos(photosArr, parseInt(element.id));
-        });
+const openPhoto = photosArr => {
+  let arr = document.querySelectorAll(".container");
+  arr.forEach(element => {
+    element.addEventListener("click", event => {
+      event.preventDefault();
+      paintBrandingPhotos(photosArr, parseInt(element.id));
     });
-}
+  });
+};
 
 /* --------------------------- For branding section -------------------------------- */
 
 const handleBranding = arr => {
-    gallery.innerHTML = '';
-    const brandingCategories = `
+  gallery.innerHTML = "";
+  const brandingCategories = `
     <div class="widest branding blueB" id="publicaciones">Publications</div>
     <div class="taller branding blackB" id="cocacola">Coca-Cola</div>
     <div class="small branding blueB" id="capufe">CAPUFE</div>
     <div class="small branding blueB" id="kellogs">Kellogg's</div>
-    `
-    gallery.insertAdjacentHTML('beforeend', brandingCategories);
-    const brandingCategoriesArr = document.querySelectorAll('.branding');
-    handlingCategoriesOnClick(brandingCategoriesArr, arr);
+    `;
+  gallery.insertAdjacentHTML("beforeend", brandingCategories);
+  const brandingCategoriesArr = document.querySelectorAll(".branding");
+  handlingCategoriesOnClick(brandingCategoriesArr, arr);
 };
 
 const handlingCategoriesOnClick = (categoriesCollection, photosCollection) => {
-    categoriesCollection.forEach(category => {
-        category.addEventListener('click', event => {
-            event.preventDefault();
-            filteringBrandinPhotos(category.id, photosCollection);
-        });
+  categoriesCollection.forEach(category => {
+    category.addEventListener("click", event => {
+      event.preventDefault();
+      filteringBrandinPhotos(category.id, photosCollection);
     });
+  });
 };
 
 const filteringBrandinPhotos = (id, photos) => {
-    let filteredArr = photos.filter(photo => photo.subcategory === id);
-    paintBrandingPhotos(filteredArr, 0);
+  let filteredArr = photos.filter(photo => photo.subcategory === id);
+  paintBrandingPhotos(filteredArr, 0);
 };
 
 /* --------------------------- Modal of branding section + carousel -------------------------------- */
 
 const paintBrandingPhotos = (arr, id) => {
-    modal.classList.add('active');
-    let imgArray = arr.map((img) => {
-        return `<img class="mySlides" id="${img.num}" src="${img.url}" style="width:100%; height: 100%; display:none; object-fit: contain;"/>`
-    });
-    loopingImages(imgArray, id);
-    closeModal();
+  modal.classList.add("active");
+  let imgArray = arr.map(img => {
+    return `<img class="mySlides" id="${img.num}" src="${img.url}" style="width:100%; height: 100%; display:none; object-fit: contain;"/>`;
+  });
+  loopingImages(imgArray, id);
+  closeModal();
 };
 
 const closeModal = () => {
-    closeModalBttn.addEventListener('click', () => {
-        modal.classList.remove('active');
-        carousel.innerHTML = '';
-        slideIndex = '';
-    })
+  closeModalBttn.addEventListener("click", () => {
+    modal.classList.remove("active");
+    carousel.innerHTML = "";
+    slideIndex = "";
+  });
 };
 
 let slides;
 let slideLen;
 
 function loopingImages(arr, id) {
-    arr.forEach(element => {
-        carousel.insertAdjacentHTML('beforeend', element);
-    });
-    slides = document.querySelectorAll('.mySlides');
-    showSlides(id);
-};
-
+  arr.forEach(element => {
+    carousel.insertAdjacentHTML("beforeend", element);
+  });
+  slides = document.querySelectorAll(".mySlides");
+  showSlides(id);
+}
 
 function showSlides(id) {
-    slideLen = slides.length - 1;
-    slideIndex = id;
-    checkingSlideIndex(slideIndex, slideLen, slides);
-};
+  slideLen = slides.length - 1;
+  slideIndex = id;
+  checkingSlideIndex(slideIndex, slideLen, slides);
+}
 
-
-    prev.addEventListener('click', (event) => {
-        event.preventDefault();
-        checkingSlideIndex(slideIndex += -1, slideLen, slides);
-    });
-    next.addEventListener('click', (event) => {
-        event.preventDefault();
-        checkingSlideIndex(slideIndex += 1, slideLen, slides);
-    });
+prev.addEventListener("click", event => {
+  event.preventDefault();
+  checkingSlideIndex((slideIndex += -1), slideLen, slides);
+});
+next.addEventListener("click", event => {
+  event.preventDefault();
+  checkingSlideIndex((slideIndex += 1), slideLen, slides);
+});
 
 function checkingSlideIndex(n, len, arr) {
-    let forCheckingIndex = n;
-    if (n > len) {
-        forCheckingIndex = 0;
-        slideIndex = 0;
-        paintSlide(forCheckingIndex, arr);
-    } else if (n < 0) {
-        forCheckingIndex = len;
-        slideIndex = len;
-        paintSlide(forCheckingIndex, arr)
-    } else {
-        paintSlide(forCheckingIndex, arr)
-    }
-    counter.innerHTML = `${slideIndex+1}/${slideLen+1}`
+  let forCheckingIndex = n;
+  if (n > len) {
+    forCheckingIndex = 0;
+    slideIndex = 0;
+    paintSlide(forCheckingIndex, arr);
+  } else if (n < 0) {
+    forCheckingIndex = len;
+    slideIndex = len;
+    paintSlide(forCheckingIndex, arr);
+  } else {
+    paintSlide(forCheckingIndex, arr);
+  }
+  counter.innerHTML = `${slideIndex + 1}/${slideLen + 1}`;
 }
 
 function paintSlide(slideIndex, arr) {
-    for (let i = 0; i < arr.length; i++) {
-        if (i === slideIndex) {
-            arr[slideIndex].style.display = "block";
-        } else {
-            arr[i].style.display = "none";
-        }
+  for (let i = 0; i < arr.length; i++) {
+    if (i === slideIndex) {
+      arr[slideIndex].style.display = "block";
+    } else {
+      arr[i].style.display = "none";
     }
+  }
 }
 
 /* --------------------------- Function that changes link color when is activated -------------------------------- */
 
 const coloringLinksCategories = (arr, link) => {
-    arr.forEach(element => {
-        if (element.classList.contains('active')) {
-            element.classList.remove('active');
-        }
-    });
-    link.classList.add('active');
+  arr.forEach(element => {
+    if (element.classList.contains("active")) {
+      element.classList.remove("active");
+    }
+  });
+  link.classList.add("active");
 };
 
 /* --------------------------- Function that triggers the fetch function -------------------------------- */
 
 photoCategoriesLinks.forEach(category => {
-    category.addEventListener('click', event => {
-        event.preventDefault();
-        obtainingJsonData(event.target.id);
-        coloringLinksCategories(photoCategoriesLinks, event.target);
-    });
+  category.addEventListener("click", event => {
+    event.preventDefault();
+    obtainingJsonData(event.target.id);
+    coloringLinksCategories(photoCategoriesLinks, event.target);
+  });
 });
 
-portfolioLink.addEventListener('click', () => {
-    obtainingJsonData('fashion');
-    coloringLinksCategories(photoCategoriesLinks, fashionLink);
+portfolioLink.addEventListener("click", () => {
+  obtainingJsonData("fashion");
+  coloringLinksCategories(photoCategoriesLinks, fashionLink);
+  socialListone.classList.remove("hide");
 });
 
-scrollButton.addEventListener('click', () => {
-    obtainingJsonData('fashion');
+scrollButton.addEventListener("click", () => {
+  obtainingJsonData("fashion");
 });
--- src/js/index.js
+++ src/js/index.js
@@ -1,188 +1,216 @@
 /* ----------------- General elements ---------------------- */
 
-const sectionLinksObj = document.querySelectorAll('.section-links');
-const appPages = document.querySelectorAll('.pages');
+const sectionLinksObj = document.querySelectorAll(".section-links");
+const appPages = document.querySelectorAll(".pages");
 
 /* ----------------- Menu elements ---------------------- */
 
-const header = document.querySelector('header');
-const openMenuIcon = document.getElementById('open-menu-icon');
-const closeMenuIcon = document.getElementById('close-menu-icon');
-const hamburgerIconSvgPath1 = document.getElementById('hamburger-svg-path-1');
-const hamburgerIconSvgPath2 = document.getElementById('hamburger-svg-path-2');
-const menu = document.getElementById('menu');
-const logoFilling = document.getElementById('logo-filling');
-const logoStroke = document.getElementById('logo-stroke');
-const iconPathOne = document.querySelectorAll('.icon-path-one');
-const iconPathTwo = document.querySelectorAll('.icon-path-two');
-const sectionLinksContainer = document.querySelectorAll('.section-links-container a');
-const logo = document.getElementById('logo');
-const homeLink = document.getElementById('menu-home-link');
-const socialMediaHeader = document.querySelectorAll('.social-media-header');
-const portfolioSection = document.getElementById('portfolio');
+const header = document.querySelector("header");
+const openMenuIcon = document.getElementById("open-menu-icon");
+const closeMenuIcon = document.getElementById("close-menu-icon");
+const hamburgerIconSvgPath1 = document.getElementById("hamburger-svg-path-1");
+const hamburgerIconSvgPath2 = document.getElementById("hamburger-svg-path-2");
+const menu = document.getElementById("menu");
+const logoFilling = document.getElementById("logo-filling");
+const logoStroke = document.getElementById("logo-stroke");
+const iconPathOne = document.querySelectorAll(".icon-path-one");
+const iconPathTwo = document.querySelectorAll(".icon-path-two");
+const sectionLinksContainer = document.querySelectorAll(
+  ".section-links-container a"
+);
+const logo = document.getElementById("logo");
+const homeLink = document.getElementById("menu-home-link");
+const contactLink = document.getElementById("menu-contact-link");
+const socialList = document.getElementById("social-list");
+const aboutLink = document.getElementById("about-link");
+const filmLink = document.getElementById("film-link");
+const socialMediaHeader = document.querySelectorAll(".social-media-header");
+const portfolioSection = document.getElementById("portfolio");
 /* ----------------- Home elements ---------------------- */
 
-const homeSection = document.getElementById('home');
-const scrollBttn = document.getElementById('scroll-bttn');
+const homeSection = document.getElementById("home");
+const scrollBttn = document.getElementById("scroll-bttn");
 
 /* ----------------- Footer elements ---------------------- */
 
-const footer = document.querySelector('footer');
+const footer = document.querySelector("footer");
 
 /* ----------------- Functions ---------------------- */
 
 function hamburgerIconColor() {
-    let iconDisplayStyle = window.getComputedStyle(homeSection, null).getPropertyValue("display");
-    if (iconDisplayStyle === 'grid') {
-        hamburgerIconSvgPath1.setAttribute('stroke', '#FEFEFE');
-        hamburgerIconSvgPath2.setAttribute('fill', '#FFFFFF');
-    } else if (iconDisplayStyle === 'none') {
-        hamburgerIconSvgPath1.setAttribute('stroke', '#272424');
-        hamburgerIconSvgPath2.setAttribute('fill', '#000000');
-    }
+  let iconDisplayStyle = window
+    .getComputedStyle(homeSection, null)
+    .getPropertyValue("display");
+  if (iconDisplayStyle === "grid") {
+    hamburgerIconSvgPath1.setAttribute("stroke", "#FEFEFE");
+    hamburgerIconSvgPath2.setAttribute("fill", "#FFFFFF");
+  } else if (iconDisplayStyle === "none") {
+    hamburgerIconSvgPath1.setAttribute("stroke", "#272424");
+    hamburgerIconSvgPath2.setAttribute("fill", "#000000");
+  }
 }
 hamburgerIconColor();
 
 let w =
-    document.documentElement.clientWidth ||
-    document.body.clientWidth ||
-    window.innerWidth;
+  document.documentElement.clientWidth ||
+  document.body.clientWidth ||
+  window.innerWidth;
 
 window.addEventListener(
-    "resize",
-    () => {
-        w =
-            document.documentElement.clientWidth ||
-            document.body.clientWidth ||
-            window.innerWidth;
-        listeningScreenWidth(w);
-    },
-    false
+  "resize",
+  () => {
+    w =
+      document.documentElement.clientWidth ||
+      document.body.clientWidth ||
+      window.innerWidth;
+    listeningScreenWidth(w);
+  },
+  false
 );
 
 function listeningScreenWidth(w) {
-    if (w <= 1024) {
-        homeLink.style.display = 'block';
-        if (homeSection.classList.contains('active')) {
-            isHomeActiveMobile();
-        } else {
-            isntHomeActive();
-        }
+  if (w <= 1024) {
+    homeLink.style.display = "block";
+    if (homeSection.classList.contains("active")) {
+      isHomeActiveMobile();
     } else {
-        homeLink.style.display = 'none';
-        if (homeSection.classList.contains('active')) {
-            isHomeActiveDesktop();
-        } else {
-            isntHomeActive();
-        }
+      isntHomeActive();
     }
-};
+  } else {
+    homeLink.style.display = "none";
+    if (homeSection.classList.contains("active")) {
+      isHomeActiveDesktop();
+    } else {
+      isntHomeActive();
+    }
+  }
+}
 
 listeningScreenWidth(w);
 
 function isHomeActiveMobile() {
-    header.style.background = 'transparent';
-    footer.style.display = 'none';
-    logoFilling.setAttribute('fill', '');
-    logoStroke.setAttribute('stroke', '');
-    changeIconPathColor(iconPathOne, '#000000');
-    changeIconPathColor(iconPathTwo, '#ffffff');
-    changeMenuLinksColor('black', sectionLinksContainer);
-};
+  header.style.background = "transparent";
+  footer.style.display = "none";
+  logoFilling.setAttribute("fill", "");
+  logoStroke.setAttribute("stroke", "");
+  changeIconPathColor(iconPathOne, "#000000");
+  changeIconPathColor(iconPathTwo, "#ffffff");
+  changeMenuLinksColor("black", sectionLinksContainer);
+}
 
 function isHomeActiveDesktop() {
-    header.style.background = 'transparent';
-    footer.style.display = 'none';
-    logoFilling.setAttribute('fill', '#ffffff');
-    logoStroke.setAttribute('stroke', '#ffffff');
-    changeIconPathColor(iconPathOne, '#ffffff');
-    changeIconPathColor(iconPathTwo, '#000000');
-    changeMenuLinksColor('white', sectionLinksContainer);
-};
+  header.style.background = "transparent";
+  footer.style.display = "none";
+  logoFilling.setAttribute("fill", "#ffffff");
+  logoStroke.setAttribute("stroke", "#ffffff");
+  changeIconPathColor(iconPathOne, "#ffffff");
+  changeIconPathColor(iconPathTwo, "#000000");
+  changeMenuLinksColor("white", sectionLinksContainer);
+}
 
 function isntHomeActive() {
-    header.style.background = '#ffffff';
-    footer.style.display = 'grid';
-    logoFilling.setAttribute('fill', '#000000');
-    logoStroke.setAttribute('stroke', '#000000');
-    changeIconPathColor(iconPathOne, '#000000');
-    changeIconPathColor(iconPathTwo, '#ffffff');
-    changeMenuLinksColor('black', sectionLinksContainer);
-};
+  header.style.background = "#ffffff";
+  footer.style.display = "grid";
+  logoFilling.setAttribute("fill", "#000000");
+  logoStroke.setAttribute("stroke", "#000000");
+  changeIconPathColor(iconPathOne, "#000000");
+  changeIconPathColor(iconPathTwo, "#ffffff");
+  changeMenuLinksColor("black", sectionLinksContainer);
+}
 
 function changeIconPathColor(arr, color) {
-    arr.forEach(element => {
-        element.setAttribute('fill', `${color}`);
-    });
-};
+  arr.forEach(element => {
+    element.setAttribute("fill", `${color}`);
+  });
+}
 
 function changeMenuLinksColor(color, arr) {
-    arr.forEach(element => {
-        if (element.classList.contains('white') && color != 'white') {
-            element.classList.remove('white');
-            element.classList.add(`${color}`);
-        } else if (element.classList.contains('black') && color != 'black') {
-            element.classList.remove('black');
-            element.classList.add(`${color}`);
-        } else {
-            element.classList.add(`${color}`);
-        }
-    });
-};
+  arr.forEach(element => {
+    if (element.classList.contains("white") && color != "white") {
+      element.classList.remove("white");
+      element.classList.add(`${color}`);
+    } else if (element.classList.contains("black") && color != "black") {
+      element.classList.remove("black");
+      element.classList.add(`${color}`);
+    } else {
+      element.classList.add(`${color}`);
+    }
+  });
+}
 
 function removeSocialMedia(arr, style) {
-    arr.forEach(element => {
-        element.style.display = `${style}`;
-    })
-};
+  arr.forEach(element => {
+    element.style.display = `${style}`;
+  });
+}
 
 /* ----------------- DOM events ---------------------- */
 
-openMenuIcon.addEventListener('click', (event) => {
-    event.preventDefault();
-    menu.classList.add('active');
+openMenuIcon.addEventListener("click", event => {
+  event.preventDefault();
+  menu.classList.add("active");
 });
 
-closeMenuIcon.addEventListener('click', () => {
-    event.preventDefault();
-    menu.classList.remove('active');
+closeMenuIcon.addEventListener("click", () => {
+  event.preventDefault();
+  menu.classList.remove("active");
 });
 
 sectionLinksObj.forEach(element => {
-    element.addEventListener('click', (event) => {
-        event.preventDefault();
-        menu.classList.remove('active');
-        appPages.forEach(page => {
-            hamburgerIconColor();
-            listeningScreenWidth(w);
-            if (event.target.classList.contains(page.id)) {
-                page.classList.add('active');
-                window.scrollTo(0, 0);
-            } else {
-                page.classList.remove('active');
-            }
-        })
-    })
-});
-
-scrollBttn.addEventListener('click', (event) => {
+  element.addEventListener("click", event => {
     event.preventDefault();
-    const portfolioSection = document.getElementById('portfolio');
-    homeSection.classList.remove('active');
-    portfolioSection.classList.add('active');
-    hamburgerIconColor();
-    listeningScreenWidth(w);
-});
-
-logo.addEventListener('click', (event) => {
-    event.preventDefault();
+    menu.classList.remove("active");
     appPages.forEach(page => {
-        if (page.id === 'home') {
-            page.classList.add('active');
-            hamburgerIconColor();
-            listeningScreenWidth(w);
-        } else {
-            page.classList.remove('active');
-        }
+      hamburgerIconColor();
+      listeningScreenWidth(w);
+      if (event.target.classList.contains(page.id)) {
+        page.classList.add("active");
+        window.scrollTo(0, 0);
+      } else {
+        page.classList.remove("active");
+      }
     });
+  });
 });
+
+scrollBttn.addEventListener("click", event => {
+  event.preventDefault();
+  const portfolioSection = document.getElementById("portfolio");
+  homeSection.classList.remove("active");
+  portfolioSection.classList.add("active");
+  hamburgerIconColor();
+  listeningScreenWidth(w);
+});
+
+logo.addEventListener("click", event => {
+  event.preventDefault();
+  appPages.forEach(page => {
+    if (page.id === "home") {
+      page.classList.add("active");
+      hamburgerIconColor();
+      listeningScreenWidth(w);
+    } else {
+      page.classList.remove("active");
+    }
+  });
+});
+
+const hideContact = () => {
+  socialList.classList.add("hide");
+};
+contactLink.addEventListener("click", hideContact);
+
+const contactLogo = () => {
+  socialList.classList.remove("hide");
+};
+logo.addEventListener("click", contactLogo);
+
+const contactAbout = () => {
+  socialList.classList.remove("hide");
+};
+aboutLink.addEventListener("click", contactAbout);
+
+const contactFilm = () => {
+  socialList.classList.remove("hide");
+};
+filmLink.addEventListener("click", contactFilm);
